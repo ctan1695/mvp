de79fc2c7fb85d4fb83afc590a34184a7ed48999
Chloe Tan <chloe.tan95@gmail.com>
2021-02-03 23:11:50 -0500

    Complete inserting user and recipe into db using promise chain

--- server.js
+++ server.js
@@ -32,45 +32,57 @@ app.get('/submit', (req, res) => {
 })
 
 app.post('/submit', (req, res) => {
-  console.log('req: ', req.body);
-
-  //Insert user into the database
-  query = 'insert into USERS (name) select ? where not exists (select NAME from USERS where NAME = ?);'
-  queryArgs = [req.body.user, req.body.user];
-
-  database.query(query, queryArgs, (err, results) => {
-    if (err) {
-      throw err;
-    } else {
-      console.log('Successfully inserted user into database, here are the results: ', results);
-    }
-  });
-
-  //Find our user's ID for next insert
-  var userID;
-  query = 'select ID from USERS where NAME = ?;'
-  queryArgs = [req.body.user];
-
-  database.query(query, queryArgs, (err, results) => {
-    if (err) {
-      throw err;
-    } else {
-      userID = results[0].ID;
-      console.log('Successfully queried user from database, here are the results: ', userID);
-    }
+  return new Promise((resolve, reject) => {
+    console.log('First Promise Reached!');
+    resolve('Success!');
   })
+  .then(() => {
+    return new Promise((resolve, reject) => {
+      //Insert user into the database
+      query = 'insert into USERS (name) select ? where not exists (select NAME from USERS where NAME = ?);'
+      queryArgs = [req.body.user, req.body.user];
 
-  console.log('req.body.recipe', req.body.recipe);
-  //Insert recipe into the database for our user
-  query = 'insert into RECIPES (recipe_url, user_id) values (?, ?);'
-  queryArgs = [req.body.recipe, userID];
-
-  database.query(query, queryArgs, (err, results) => {
-    if (err) {
-      throw err;
-    } else {
-      console.log('Successfully inserted recipe into database, here are the results: ', results);
-    }
+      database.query(query, queryArgs, (err, results) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results);
+        };
+      });
+    })
   })
+  .then(() => {
+    return new Promise((resolve, reject) => {
+      //Find our user's ID for next insert
+      query = 'select ID from USERS where NAME = ?;'
+      queryArgs = [req.body.user];
 
+      database.query(query, queryArgs, (err, results) => {
+        if (err) {
+          reject(err);
+        } else {
+          userID = results[0].ID;
+          console.log('userID: ', userID);
+          resolve(userID);
+        }
+      })
+    })
+  })
+  .then((user_id) => {
+    return new Promise((resolve, reject) => {
+      console.log('user_id: ', user_id);
+      //Insert recipe into the database for our user
+      query = 'insert into RECIPES (recipe_url, user_id) values (?, ?);'
+      queryArgs = [req.body.recipe, user_id];
+
+      database.query(query, queryArgs, (err, results) => {
+        if (err) {
+          reject(err);
+        } else {
+          console.log('Successfully inserted user and recipe into database');
+          resolve(results);
+        }
+      })
+    })
+  })
 });
